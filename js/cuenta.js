c07351ce267a1b01cbe8e2e0d68d89e3a03907ff
Josue <127801306+Josue644@users.noreply.github.com>
2024-09-30 10:29:23 -0500

    Update cuenta.js

--- js/cuenta.js
+++ js/cuenta.js
@@ -3,15 +3,23 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Nov 09, 2024 16:38:00').getTime(),
+let countDown = new Date('Nov 09, 2024 18:30:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
           distance = countDown - now;
 
       document.getElementById('dias').innerText = Math.floor(distance / (day)),
-        document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
+      document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
+
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById('dias').innerText = 0;
+        document.getElementById('horas').innerText = 0;
+        document.getElementById('minutos').innerText = 0;
+        document.getElementById('segundos').innerText = 0;
+    }
 
     }, second)
